e1fe1a913724832e967b864397ef8c8642dca0a9
fryerd1 <dylan.fryer@ons.gov.uk>
2026-01-19 15:09:23 +0000

    Add explaination of __init__.py

---
+++ src/python_rap_demo/__init__.py
@@ -0,0 +1,5 @@
+# What is __init__.py?
+#
+# The __init__.py file is used to mark a directory as a Python package.
+# This means that the files in the /src folder and /python_rap_demo
+# can be imported and used in other Python scripts in the python_rap_demo.
